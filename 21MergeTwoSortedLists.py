a718b2527a0da637a9e624cf07d0b9317ee3c63c
xylaughing <90731147+xylaughing@users.noreply.github.com>
2021-09-17 23:17:22 -0700

    Create 21MergeTwoSortedLists.py

---
+++ 21MergeTwoSortedLists.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        result = ListNode()
+        listhead = result
+        while l1 != None and l2 != None:
+            if l1.val >= l2.val:
+                result.next = l2
+                l2 = l2.next
+            else:
+                result.next = l1
+                l1 = l1.next
+            result = result.next
+                
+        if l1 != None:
+            result.next = l1
+        if l2 != None:
+            result.next = l2
+        return listhead.next
