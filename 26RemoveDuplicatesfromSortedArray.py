4f915dd2e20cfb22aa5fbba8cb74e787cfaa267e
xylaughing <90731147+xylaughing@users.noreply.github.com>
2021-09-19 00:00:21 -0700

    Update 26RemoveDuplicatesfromSortedArray.py

--- 26RemoveDuplicatesfromSortedArray.py
+++ 26RemoveDuplicatesfromSortedArray.py
@@ -3,12 +3,9 @@
         :type nums: List[int]
         :rtype: int
         """
-        k = len(nums)
-        i = j = 1       # two pointer i and j
-        for j in range(1, len(nums), 1):
-            if nums[j] == nums[j-1]:    
-                k = k - 1
-            else:
-                nums[i] = nums[j]
-                i = i + 1
+        k = 1       # two pointer k and j
+        for i in range(1, len(nums), 1):
+            if nums[i] != nums[k-1]:
+                nums[k] = nums[i]
+                k += 1
         return k
