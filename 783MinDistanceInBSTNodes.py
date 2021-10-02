ed58770a540382fba693c86cba69ab3aee529111
xylaughing <90731147+xylaughing@users.noreply.github.com>
2021-10-02 15:57:10 -0700

    Create 783MinDistanceInBSTNodes.py

---
+++ 783MinDistanceInBSTNodes.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def minDiffInBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        def preorder(root):
+            if root is None: return []            
+            return preorder(root.left) + [root.val] + preorder(root.right)
+        
+        arr = preorder(root)
+        for i in range(len(arr)-1):
+            temp = arr[i+1] - arr[i]
+            if temp < 0: arr[i] = - temp
+            else: arr[i] = temp
+        
+        return min(arr[0:len(arr)-1])
