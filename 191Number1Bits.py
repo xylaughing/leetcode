6920d1d142da067f7f83949e8759e9b8408ae51a
xylaughing <90731147+xylaughing@users.noreply.github.com>
2021-10-08 11:04:45 -0700

    Update 191Number1Bits.py

--- 191Number1Bits.py
+++ 191Number1Bits.py
@@ -4,8 +4,17 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        """ Option 1
         result = 0
         while n != 0:
             result += n%2 
             n = n // 2
         return result
+        """
+        
+        # Option 2
+        res = 0
+        for i in range(32):
+            res += n&1
+            n >>= 1
+        return res
