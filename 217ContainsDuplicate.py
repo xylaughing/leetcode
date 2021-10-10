292dabeb6160ffa25d7e41e2eb13588b95713dbe
xylaughing <90731147+xylaughing@users.noreply.github.com>
2021-10-09 23:26:32 -0700

    Create 217ContainsDuplicate.py

---
+++ 217ContainsDuplicate.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        dic = collections.Counter(nums)
+        for x in dic.values():
+            if x != 1: return True
+        return False
