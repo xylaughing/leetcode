dd53b44d04b71aff5b60a775b117025ba68390ca
xylaughing <90731147+xylaughing@users.noreply.github.com>
2021-10-11 22:22:59 -0700

    Create 220ContainsDuplicateIII.py

---
+++ 220ContainsDuplicateIII.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def containsNearbyAlmostDuplicate(self, nums, k, t):
+        """
+        :type nums: List[int]
+        :type k: int
+        :type t: int
+        :rtype: bool
+        """
+        
+        bucket = {}
+        for i, num in enumerate(nums):
+            if len(bucket)>k:
+                bidToRemove = nums[i-k-1]//(t+1)
+                del bucket[bidToRemove]
+            
+            bid = num//(t+1)
+            if bid in bucket: return True
+            if bid+1 in bucket and abs(bucket[bid+1]-num)<=t: return True
+            if bid-1 in bucket and abs(bucket[bid-1]-num)<=t: return True
+            bucket[bid] = num
+        return False
+                                  
