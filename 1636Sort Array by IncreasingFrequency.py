110cdf3cb0e99d21d7c1f933cd3cbfe92a326fbf
xylaughing <90731147+xylaughing@users.noreply.github.com>
2021-10-15 10:50:35 -0700

    Create 1636Sort Array by IncreasingFrequency.py

---
+++ 1636Sort Array by IncreasingFrequency.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def frequencySort(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        dic, res = {}, []
+        ct = Counter(nums)
+        for x in ct:
+            if ct[x] not in dic: 
+                dic[ct[x]] = [x]
+            else: 
+                dic[ct[x]].append(x)
+                
+        sortedFre = sorted(dic.keys())
+        for i in sortedFre:
+            for j in sorted(dic[i], reverse = True):
+                 res.extend([j] * i)
+        return res
