e2e92743c82dfb6871e1a27015325fe04e2ab08c
xylaughing <90731147+xylaughing@users.noreply.github.com>
2021-10-25 11:04:08 -0700

    Update 28ImplementstrStr.py

--- 28ImplementstrStr.py
+++ 28ImplementstrStr.py
@@ -5,10 +5,13 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
+        """
         if len(needle) == 0:
             return 0
         for i in range(len(haystack) - len(needle) + 1):
             if needle == haystack[i : i+len(needle)]:
                 return i
         return -1
-            
+        """    
+        if needle in haystack: return haystack.find(needle)
+        else: return -1
