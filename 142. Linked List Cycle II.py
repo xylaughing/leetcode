777849b83509f0552236d063e88981467e8838e7
xylaughing <90731147+xylaughing@users.noreply.github.com>
2021-11-03 10:51:16 -0700

    Create 142. Linked List Cycle II.py

---
+++ 142. Linked List Cycle II.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        slow = fast = head
+        # this to prove it has cycle
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next  
+            if slow == fast: break
+        else:
+            return None
+        # this to determin the cycle begin
+        fast = head
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+        return fast
+            
