1d4491630551ba9dc2911d9dc54f6002c7a81343
xylaughing <90731147+xylaughing@users.noreply.github.com>
2021-11-17 11:47:43 -0800

    Create 917. Reverse Only Letters.py

---
+++ 917. Reverse Only Letters.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def reverseOnlyLetters(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        s = list(s)
+        l, r = 0, len(s)-1
+        while l < r:
+            if not s[l].isalpha():
+                l += 1
+            elif not s[r].isalpha():
+                r -= 1
+            else:
+                s[l], s[r] = s[r], s[l]
+                l, r = l+1, r-1
+        return "".join(s)
