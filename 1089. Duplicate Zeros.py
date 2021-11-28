bb7a56bb7a23a0ab9adbe66f639c5d0db8c9ec97
xylaughing <90731147+xylaughing@users.noreply.github.com>
2021-11-27 21:19:55 -0800

    Update 1089. Duplicate Zeros.py

--- 1089. Duplicate Zeros.py
+++ 1089. Duplicate Zeros.py
@@ -7,9 +7,9 @@ class Solution(object):
         n = len(arr)
         zeros = arr.count(0)
         for i in range(n-1, -1, -1):
-            if i + zeros < n:        
-                arr[i + zeros] = arr[i]
             if arr[i] == 0:
-                zeros -= 1
                 if i + zeros < n:
                     arr[i + zeros] = 0
+                zeros -= 1
+            if i + zeros < n:        
+                arr[i + zeros] = arr[i]
