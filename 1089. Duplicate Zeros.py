6c91463ccb234763c2b0b528c5233cd6e8388ed6
xylaughing <90731147+xylaughing@users.noreply.github.com>
2021-11-27 20:43:19 -0800

    Create 1089. Duplicate Zeros.py

---
+++ 1089. Duplicate Zeros.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def duplicateZeros(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: None Do not return anything, modify arr in-place instead.
+        """
+        n = len(arr)
+        zeros = arr.count(0)
+        for i in range(n-1, -1, -1):
+            if i + zeros < n:        
+                arr[i + zeros] = arr[i]
+            if arr[i] == 0:
+                zeros -= 1
+                if i + zeros < n:
+                    arr[i + zeros] = 0
