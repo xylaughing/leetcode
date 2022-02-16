ad15c83090333e72609c480262ce3c017822d211
xylaughing <90731147+xylaughing@users.noreply.github.com>
2022-02-15 22:55:41 -0800

    Create Description Solution Discuss (999+) Submissions 173. Binary Search Tree Iterator.py

---
+++ Description Solution Discuss (999+) Submissions 173. Binary Search Tree Iterator.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class BSTIterator(object):
+
+    def __init__(self, root):
+        """
+        :type root: TreeNode
+        """
+        self.stack = []
+        while root:
+            self.stack.append(root)
+            root = root.left
+            
+    def next(self):
+        """
+        :rtype: int
+        """
+        curnode = self.stack.pop()
+        node = curnode.right
+        while node:
+            self.stack.append(node)
+            node = node.left
+        return curnode.val
+        
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        return len(self.stack) > 0
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
